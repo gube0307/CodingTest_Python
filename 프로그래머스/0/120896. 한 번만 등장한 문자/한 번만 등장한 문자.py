5d29205cf787eb7b1248b3163444ba801a1cb3dd
gube0307 <165009462+gube0307@users.noreply.github.com>
2024-11-29 11:50:57 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.33 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = ''
+    for i in s:
+        a = 0
+        for j in s:
+            if i == j:
+                a += 1
+        if a == 1:
+            answer += i
+    return ''.join(sorted(answer))
